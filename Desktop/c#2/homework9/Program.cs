026f701f21d765874b8e45df87fcde2ac3ea57cb
Bet1nka <betina96@mail.ru>
2022-05-24 21:17:06 +0300

    first little bit better now

--- Desktop/c#2/homework9/Program.cs
+++ Desktop/c#2/homework9/Program.cs
@@ -1,22 +1,34 @@
 ﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-/*int SumOfIntNumbers(int m, int n)
-{
-    if (m == n)
-    {
-        return n;
-    }
-    return n + SumOfIntNumbers(m, n - 1);
+int SumOfIntNumbers(int m, int n)
+{ 
+        int temp =0;
+        if(n<m)
+        {
+            temp=n;
+            n=m;
+            m=temp;
+        }
+        if (m == n)
+         {
+              return n;
+          }
+          return n + SumOfIntNumbers(m,n - 1);
 }
-Console.Write(SumOfIntNumbers(3, 9));
+Console.WriteLine("input your first number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input your second number: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write(SumOfIntNumbers(x,y));
 
 //Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
-int SumOfDigits(int a)
+/*int SumOfDigits(int a)
 {
-  if(a/10!=0)
-  {
-  return a%10+SumOfDigits(a/10);
-  }
-  return a%10;
+    if (a / 10 != 0)
+    {
+        return a % 10 + SumOfDigits(a / 10);
+    }
+    return a % 10;
 }
-Console.WriteLine(SumOfDigits(345));
-*/
+Console.WriteLine("input your number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("sum of digits is -> " + SumOfDigits(x));*/
