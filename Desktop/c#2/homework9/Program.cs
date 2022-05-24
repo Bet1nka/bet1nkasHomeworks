f9f26f675de3b8fa03466be93586ad3d93488752
Bet1nka <betina96@mail.ru>
2022-05-24 19:11:45 +0300

    nakonec to

---
+++ Desktop/c#2/homework9/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+/*int SumOfIntNumbers(int m, int n)
+{
+    if (m == n)
+    {
+        return n;
+    }
+    return n + SumOfIntNumbers(m, n - 1);
+}
+Console.Write(SumOfIntNumbers(3, 9));
+
+//Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+int SumOfDigits(int a)
+{
+  if(a/10!=0)
+  {
+  return a%10+SumOfDigits(a/10);
+  }
+  return a%10;
+}
+Console.WriteLine(SumOfDigits(345));
+*/
